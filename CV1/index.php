09ed761dfbce899873d9492f84d5922cf2d60e6c
speedyvoodoo420 <kebalex@hotmail.fr>
2020-09-17 18:16:11 +0200

    First commit CV

---
+++ CV1/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CV Alexandre Kebaili</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="css/resume.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Alexandre KEBAILI</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/portrait_Alex.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">A propos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#experiences">Experiences</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#formation">Formation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#interets">Interets</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#certificats">Certificats</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="nav-link js-scroll-trigger">
+    <p>Téléchargez le CV en PDF :
+    <a class="" href="documents/AlexandreKebailiSombre.pdf" download="CVAlexandreKebailiSombre.pdf">Sombre</a> /
+    <a class="" href="documents/AlexandreKebailiClaire.pdf" download="CVAlexandreKebailiClaire.pdf">Claire</a>
+  </p>  
+</div>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-0">Alexandre
+          <span class="text-primary">KEBAILI</span>
+        </h1>
+        <div class="subheading mb-5">3 place du colombier, Lieusaint
+          <a href="mailto:name@email.com">a.kebaili.h@gmail.com</a>
+        </div>
+        <p class="lead">
+          Apres une reconversion réussie en développement informatique et l'obtention d'un BTS SIO option SLAM en un an je me dirige désormais vers la poursuite d'études en Licence Cloud et internet des objets.</p>
+        <p class="lead mb-5">
+        Passionné d'informatique et fort de deux années d'expériences à l'étranger, je saurais apporter mon dynamisme, mon autonomie ainsi que ma capacité d'adaptation aux projets professionnels.</p>
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/in/alexandre-kebaili" target="_blank">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://github.com/AlexandreKebaili" target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experiences">
+      <div class="w-100">
+        <h2 class="mb-5">Experiences</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Piscine école 42</h3>
+            <div class="subheading mb-3">Ecole 42</div>
+            <ul>
+              <li>Atteinte du niveau 6 (+230h au sein de l'école)</li>
+              <li>Travail en groupe durant les week-ends</li>
+              <li>Commandes Shell et programmation en C</li>
+            </ul>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2020</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Stage en entreprise</h3>
+            <div class="subheading mb-3">AntemetA / TF</div>
+            <ul>
+              <li>Participation à un projet</li>
+              <li>Analyse du cahier des charges d'un service à produire</li>
+              <li>Conception d'un logiciel et d'un site de reservation de trajets</li>
+            </ul>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2019 - 2020</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Préparateur de commandes</h3>
+            <div class="subheading mb-3">SAS Nature / Castorama / Logidis</div>
+            <ul>
+              <li>Picking en vocal</li>
+              <li>Préparation et envoi de colis</li>
+              <li>Conduite d'engins</li>
+            </ul>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2016 - 2018</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Expériences à l’étranger</h3>
+            <div class="subheading mb-3">Canada : Montréal Poutine/Frite Alors !
+              <br>
+              Australie : Martin Place Bar/ Sydney Opéra Bar</div>
+            <ul>
+              <li>Service à table</li>
+              <li>Ouverture/Fermeture du restaurant</li>
+              <li>Flux de clientèle important</li>
+            </ul>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2013 - 2016</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+          <div class="resume-content">
+            <h3 class="mb-0">Responsable opérationnel</h3>
+            <div class="subheading mb-3">McDonald’s</div>
+            <ul>
+              <li>Organisations et gestion d'une zone</li>
+              <li>Service et accueil clients</li>
+              <li>Assurer le respect des normes, production</li>
+            </ul>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2009 - 2012</span>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="formation">
+      <div class="w-100">
+        <h2 class="mb-5">Formation</h2>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">UPEC Creteil Paris-Est</h3>
+            <div class="subheading mb-3">Licence Cloud et Internet des Objets</div>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2020- 2021</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">GRETA METEHOR ENC Paris</h3>
+            <div class="subheading mb-3">BTS SIO option SLAM</div>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2019 - 2020</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Polytheig (alternance)</h3>
+            <div class="subheading mb-3">BTS MUC (niveau)</div>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2010 - 2012</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Lycée la tour des Dames</h3>
+            <div class="subheading mb-3">BAC STG option CFE</div>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2008 -2011</span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interets">
+      <div class="w-100">
+        <h2 class="mb-5">Interets</h2>
+        <p>
+          Passionné depuis toujours par le monde de l'informatique, c'est naturellement que je me suis tourné vers le monde du développement, notamment le développement de logiciels et d'application web.
+          <br>
+          Ayant pratiqué le Bicross pendant de longues années mon attirance envers les sports extrêmes a pu entretenir mon esprit de compétition.
+          <br><br>
+          En dehors de mon bureau j'aime pratiquer des activités manuelles telles que la mécanique automobile ou encore de simple ballades pour profité des paysages exceptionelles de la France.
+          Et lorsque le temps ne le permet pas je me plais à lire de nombreuses oeuvres fantastiques ou encore simplement regarder des films ou bien sûr jouer aux jeux vidéo.
+          <br>
+          <div class="container" style="height=100px;"></div>
+          Les voyages ont longtemps été d'un grand interêt et j'ai eu la chance de pouvoir vivre une année dans deux pays différents: le Canada et l'Australie.
+          <br><div class="container"></div>
+          Au Canada j'ai pu profiter de la vie montréalaise pendant plus de 6 mois pour ensuite traverser le Nord des États-Unis afin de vivre les 4 derniers mois de mon visa dans la station de ski de Whistler au Nord de Vancouver.
+          <br>
+          En Australie l'achat d'un van équipé a permis plus de déplacements, permettant de profiter de tout le côté Est Australien, du Nord au Sud, passant de Cairns à Melbourne ou encore de Brisbane à Katherine.
+          <br><div class="container"></div>
+          Les paysages époustouflants de ces deux pays resteront longtemps dans ma mémoire et il me tarde de pouvoir créé de nouvelles aventures de la sorte.
+        </p>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="certificats">
+
+
+      <div class="w-100">
+        <h2 class="mb-5">Certificats</h2>
+
+        <div class="resume-content">      
+          <div class="resume-content">
+            <h3 class="mb-0">Cisco NetAcad</h3>
+          </div>
+          <br>
+          <ul class="">
+            <li>
+              <i class="fa-li fa text-warning"></i>
+              Cybersecurity Essentials</li>
+            </ul>
+        </div>
+        <br>
+        <div class="resume-content">
+           <h3 class="mb-0">OpenClassRooms</h3>
+         </div>
+         <br>
+         <ul class="">
+          <li>
+            <i class="fa-li fa text-warning"></i>
+            Apprenez à développer en C#</li>
+          <li>
+            <i class="fa-li fa text-warning"></i>
+            Concevez votre site avec PHP et MySQL</li>
+          <li>
+            <i class="fa-li fa text-warning"></i>
+            Adoptez une architecture MVC en PHP</li>
+          <li>
+            <i class="fa-li fa text-warning"></i>
+            Découvrez le framework PHP Laravel</li>
+          <li>
+            <i class="fa-li fa text-warning"></i>
+            Prenez en main Bootstrap</li>
+          <li>
+            <i class=""></i>
+            Apprenez le fonctionnement des réseaux TCP/IP (en cours).</li>
+         </ul>
+      </div>
+    </section>
+
+
+    <div class="nav-link js-scroll-trigger">
+  <p>Téléchargez le CV en PDF :
+  <a class="" href="documents/AlexandreKebailiSombre.pdf" download="CVAlexandreKebailiSombre.pdf">Sombre</a> /
+  <a class="" href="documents/AlexandreKebailiClaire.pdf" download="CVAlexandreKebailiClaire.pdf">Claire</a>
+  </p>  
+</div>
+
+  </div>
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/resume.min.js"></script>
+
+</body>
+
+</html>
